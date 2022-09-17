e70070778efef5387c558239387638c402c0b779
Alex Muller <alexmullerdesign@gmail.com>
2022-09-18 00:12:58 +0300

    connect to Supabase

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it bro!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Profile v-if="store.user" />
+    <Auth v-else />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import { store } from "./store";
+import { supabase } from "./supabase";
+import Auth from "./components/Auth.vue";
+import Profile from "./components/Profile.vue";
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  components: {
+    Auth,
+    Profile,
+  },
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+  setup() {
+    store.user = supabase.auth.user();
+    supabase.auth.onAuthStateChange((_, session) => {
+      store.user = session.user;
+    });
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+    return {
+      store,
+    };
+  },
+};
+</script>
