d72f5eef51b022dbfd6445fcc225e75d0cff2f26
Alex Muller <alexmullerdesign@gmail.com>
2022-09-18 19:21:27 +0300

    add Three.js

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -36,6 +36,7 @@ import { supabase } from "../supabase";
 import { store } from "../store";
 import { onMounted, ref } from "vue";
 import Avatar from "./Avatar.vue";
+import * as THREE from "three";
 
 export default {
   setup() {
@@ -108,6 +109,54 @@ export default {
 
     onMounted(() => {
       getProfile();
+
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
+      document.body.appendChild(renderer.domElement);
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1);
+      var geo = new THREE.EdgesGeometry(geometry); // or WireframeGeometry( geometry )
+
+      // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const material = new THREE.LineBasicMaterial({
+        color: 0xffffff,
+        linewidth: 3,
+        linecap: "round", //ignored by WebGLRenderer
+        linejoin: "round", //ignored by WebGLRenderer
+      });
+      // const cube = new THREE.Mesh(geometry, material);
+      const cube = new THREE.LineSegments(geo, material);
+
+      const material1 = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+      const points = [];
+      points.push(new THREE.Vector3(-10, 0, 0));
+      points.push(new THREE.Vector3(0, 10, 0));
+      points.push(new THREE.Vector3(10, 0, 0));
+
+      const geometry1 = new THREE.BufferGeometry().setFromPoints(points);
+      const line = new THREE.Line(geometry1, material1);
+
+      scene.add(line);
+      scene.add(cube);
+      camera.position.z = 5;
+
+      function animate() {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+      }
+
+      animate();
     });
 
     return {
